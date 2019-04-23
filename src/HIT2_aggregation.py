f5a77cf6704708484454edf03d0ef9aa46314ea7
kimny817 <41938281+kimny817@users.noreply.github.com>
2019-04-23 17:55:10 -0400

    Update HIT2_aggregation.py

--- src/HIT2_aggregation.py
+++ src/HIT2_aggregation.py
@@ -1,21 +1,33 @@
 import pandas as pd
-import csv
 
 def process(mturk_res):
-    input_items = mturk_res['Input.user_input']
-    input_items = list(dict.fromkeys(input_items))
+    inputData = mturk_res['Input.input']
+    inputData = list(dict.fromkeys(inputData))
 
+    count = {}
     result = []
 
-    for i in input_items:
-        t = ()
-        t += (i,)
-        for index, hit in mturk_res[mturk_res['Input.user_input'] == i].iterrows():
-            for j in range(1, 4):
-                t += (hit['Answer.link' + str(j)],)
-        result.append(t)
+    for i in inputData:
+        count[1] = 0
+        count[2] = 0
+        count[3] = 0
+        count[4] = 0
+        count[5] = 0
+        count[6] = 0
+        count[7] = 0
+        count[8] = 0
+        for index, hit in mturk_res[mturk_res['Input.input'] == i].iterrows():
+            for j in range(1, 9):
+                column = 'Answer.link' + str(j) + '.on'
+                if hit[column]:
+                    count[j] += 1
+        s = sorted(count.items(), key=lambda x:-x[1])[:3]
+        t1 = 'Input.link' + str(s[0][0])
+        t2 = 'Input.link' + str(s[1][0])
+        t3 = 'Input.link' + str(s[2][0])
+        result.append((i, hit[t1], hit[t2], hit[t3]))
 
-    return result 
+    return result
 
 
 # Your main function
@@ -23,12 +35,12 @@ def process(mturk_res):
 def main():
     # Read in CVS result file with pandas
     # PLEASE DO NOT CHANGE
-    mturk_res = pd.read_csv('HIT2 Sample Output.csv')
+    mturk_res = pd.read_csv('HIT_Output.csv')
 
-    result = process(mturk_res)
-    
-    df = pd.DataFrame(result, columns = ['user_input', 'link1', 'link2', 'link3', 'link4', 'link5', 'link6', 'link7', 'link8', 'link9'])
-    df.to_csv('HIT2Result.csv', index=False)
+    ranks = process(mturk_res)
+
+    df = pd.DataFrame(ranks, columns = ['input', 'top1', 'top2', 'top3'])
+    df.to_csv('HIT_Result.csv', index=False)
 
 if __name__ == '__main__':
     main()
